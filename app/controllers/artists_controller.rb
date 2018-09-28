a4297cd19e9e8234664506c82f1f664e3f6adf46
Katrina Pritchard <pritckat@gmail.com>
2018-09-28 02:19:35 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,6 @@
 class ArtistsController < ApplicationController
-  before_action :set_preferences, only: [:index, :new]
   def index
-    if @preferences && @preferences.artist_sort_order
-  @artists = Artist.order(name: @preferences.artist_sort_order)
-else
     @artists = Artist.all
-  end
   end
 
   def show
@@ -13,11 +8,7 @@ else
   end
 
   def new
-    if @preferences && !@preferences.allow_create_artists
-  redirect_to artists_path
-else
     @artist = Artist.new
-  end
   end
 
   def create
